f5d8ae0fd4bbd3b8a94860156c265052844d466e
Pietraceci <pietracecily@gmail.com>
2022-07-04 07:34:17 -0400

    limpando dados

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,7 @@ import News from './News'
 
 function App(){
     return(
-        <div className="container1">
-        <p className='text'>
-           Mais lidas
-        </p>  
-        <News />
 
-        <div className='text'>
-            Mais lidas
-        </div>
-        <News/>  
-
-       </div>
    )
 }
 export default App;
